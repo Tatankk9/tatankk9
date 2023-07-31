3d5b69f4a1b40e431f0d616b02ba8652f68e0015
Tatan <110239595+Tatankk9@users.noreply.github.com>
2023-07-31 15:45:46 +0300

    Update index.js
    Update Contract

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x26dbd39dd343bc91833c7c01c9923c6a43032710";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
